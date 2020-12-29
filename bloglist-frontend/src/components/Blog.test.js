0e40b728191e59d753426f0ba54e5c8a9a98f179
Ting Chen <chenting_cn@foxmail.com>
2020-12-29 21:33:56 +0200

    Exercises 5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
 describe('Blog render test', () => {
@@ -19,15 +20,22 @@ describe('Blog render test', () => {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1pY2hhZWwgQ2hhbiIsImlkIjoiNWZlODhiMTM5ZmQyYWQzZmRhNTE0ZWRkIiwiaWF0IjoxNjA5MjUyMjU4fQ.v8u0wbDHB8Y_eBD4nQxCaXbXyjhrGqY5cKd2As5ADMk',
       username: 'Michael Chan',
     }
-
-    component = render(<Blog blog={blog} user={user} />)
+    const mockHandler = jest.fn()
+    component = render(
+      <Blog blog={blog} user={user} toggleVisibility={mockHandler} />
+    )
   })
 
   test('default render the title and author of a blog', () => {
     const basicBlog = component.container.querySelector(
       '.basicContent'
     )
+    const detailedContent = component.container.querySelector(
+      '.detailedContent'
+    )
+    console.log(prettyDOM(basicBlog))
     expect(basicBlog).toHaveStyle('display: block')
+    expect(detailedContent).toHaveStyle('display: none')
     expect(basicBlog).toHaveTextContent('React patterns')
     expect(basicBlog).toHaveTextContent('Michael Chan')
   })
@@ -44,5 +52,18 @@ describe('Blog render test', () => {
       '5fe88b139fd2ad3fda514ede'
     )
     expect(basicBlog).not.toHaveTextContent(1)
+  })
+
+  test('toggle view button render blog details instead of basic information', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    const basicBlog = component.container.querySelector(
+      '.basicContent'
+    )
+    expect(basicBlog).toHaveStyle('display: none')
+    const detailedContent = component.container.querySelector(
+      '.detailedContent'
+    )
+    expect(detailedContent).toHaveStyle('display: block')
   })
 })
